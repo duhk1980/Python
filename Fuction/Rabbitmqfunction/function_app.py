c1bca1ce9129f4032a22ff6ebec93438ba0d6fd7
duhk1980 <duhk1980@gmail.com>
2025-02-21 15:23:19 -0500

    rabbit mq

--- Fuction/Rabbitmqfunction/function_app.py
+++ Fuction/Rabbitmqfunction/function_app.py
@@ -1,25 +1,45 @@
 import azure.functions as func
+import datetime
+import json
 import logging
+import pika
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.FunctionApp()
 
-@app.route(route="HttpExample")
-def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="HttpToRabbitMQ", auth_level=func.AuthLevel.ANONYMOUS)
+def HttpToRabbitMQ(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    # Read the message from the HTTP request
+    message = req.get_json().get('message')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+    if not message:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "Please pass a message in the request body",
+            status_code=400
         )
+    if message:
+        # Define the RabbitMQ connection parameters
+        connection_params = pika.ConnectionParameters('localhost')
+    
+        # Establish a connection to RabbitMQ
+        connection = pika.BlockingConnection(connection_params)
+        channel = connection.channel()
+
+        # Declare a queue (if it doesn't exist)
+        queue_name = 'myqueue'
+        channel.queue_declare(queue=queue_name)
+
+        # Publish the message to the queue
+        channel.basic_publish(exchange='',
+                            routing_key=queue_name,
+                            body=message)
+
+        # Close the connection
+        connection.close()
+
+        return func.HttpResponse(
+            f"Message '{message}' sent to RabbitMQ queue '{queue_name}'",
+            status_code=200
+        )
+        
